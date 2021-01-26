c29855db8b76cd2045e3da618b5d269e27fd79d8
OmniOwl <xenon373@gmail.com>
2021-01-26 23:55:30 +0100

    We can now move a capsule around by clicking

---
+++ Assets/Source/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Camera _mainCamera;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                _agent.destination = hit.point;
+            }
+        }
+    }
+}
