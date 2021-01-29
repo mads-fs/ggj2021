7796f6a9b6a71f033e371506131e0c1b5bba1e61
OmniOwl <xenon373@gmail.com>
2021-01-29 19:24:52 +0100

    Added the Interactable Script & the OnTriggerUse scripts
    Also implemented them in the player controller

--- Assets/Source/PlayerController.cs
+++ Assets/Source/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,24 +7,110 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
 {
+    public float DistanceThreshold = 1f;
     private NavMeshAgent _agent;
     private Camera _mainCamera;
+    private bool _isDestinationUse = false;
+    private Interactable _useObject = null;
+    private bool _isActive = true;
+    private bool _playerIsSuspended = false;
+    private Queue<float> _suspensions;
+
+    [Header("Debug")]
+    [SerializeField]
+    private float _distanceToDestination = 0f;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _mainCamera = Camera.main;
+        _suspensions = new Queue<float>();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (_isActive)
         {
-            RaycastHit hit;
-            if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            if (Input.GetMouseButtonDown(0))
             {
-                _agent.destination = hit.point;
+                RaycastHit hit;
+                if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+                {
+                    _agent.destination = hit.point;
+                }
+            }
+
+            _distanceToDestination = Vector3.Distance(transform.position, _agent.destination);
+
+            if (_isDestinationUse == true)
+            {
+                if (_distanceToDestination < DistanceThreshold)
+                {
+                    _useObject.OnClick?.Invoke();
+                    _isDestinationUse = false;
+                    if (_useObject.IsOneShot == true)
+                    {
+                        _useObject.SetState(false);
+                    }
+                    _useObject = null;
+                }
             }
         }
     }
+
+    /// <summary>
+    /// Use this to set whether the player can interact with the world or the <see cref="PlayerController"/> at all.
+    /// </summary>
+    /// <param name="state">The new state. Default is true.</param>
+    public void SetPlayerState(bool state) => _isActive = state;
+
+    public void SetInteractableRouting(Interactable endInteractable)
+    {
+        _isDestinationUse = true;
+        _useObject = endInteractable;
+    }
+
+    public void PrintMessage(string value) => Debug.Log(value);
+
+    #region Suspension
+    public void SuspendPlayerForSeconds(float seconds) => QueueSuspension(seconds);
+
+    private void QueueSuspension(float seconds)
+    {
+        if (_playerIsSuspended == true)
+        {
+            _suspensions.Enqueue(seconds);
+        }
+        else
+        {
+            if (_suspensions.Count == 0)
+            {
+                _suspensions.Enqueue(seconds);
+                StartNextSuspension();
+            }
+        }
+    }
+    private void StartNextSuspension()
+    {
+        if (_suspensions.Count > 0)
+        {
+            Debug.LogWarning($"Staring Suspension for {_suspensions.Peek()} seconds.");
+            StartCoroutine(SuspendPlayer(_suspensions.Dequeue(), StartNextSuspension));
+        }
+        else
+        {
+            _playerIsSuspended = false;
+            _isActive = true;
+        }
+    }
+
+    private IEnumerator SuspendPlayer(float seconds, Action callback = null)
+    {
+        _isActive = false;
+        _playerIsSuspended = true;
+        yield return new WaitForSeconds(seconds);
+        callback?.Invoke();
+        yield return null;
+    }
+    #endregion
 }
---
+++ Assets/Source/UseOnTrigger.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SphereCollider))]
+public class UseOnTrigger : MonoBehaviour
+{
+    [Tooltip("The prefab to make visible when mousing over (if any). Leave blank if nothing should be displayed when mousing over.")]
+    public GameObject ActivatedObject;
+    public bool IsOneShot = true;
+    public bool NeedsLineOfSight = false;
+    public UnityEvent OnEnter;
+    public UnityEvent OnExit;
+
+    private bool _isMouseOver = false;
+    private bool _isActive = true;
+    private Camera _mainCam;
+
+    private void Start()
+    {
+        GetComponent<SphereCollider>().isTrigger = true;
+        _mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (_isActive)
+        {
+            if (ActivatedObject != null)
+            {
+                RaycastHit[] cameraHits;
+                Ray cameraRay = _mainCam.ScreenPointToRay(Input.mousePosition);
+                if ((cameraHits = Physics.RaycastAll(cameraRay)) != null)
+                {
+                    for (int hitIndex = 0; hitIndex < cameraHits.Length; hitIndex++)
+                    {
+                        if (cameraHits[hitIndex].collider.gameObject == gameObject)
+                        {
+                            _isMouseOver = true;
+                            break;
+                        }
+                        else
+                        {
+                            _isMouseOver = false;
+                        }
+                    }
+                }
+                ActivatedObject.SetActive(_isMouseOver);
+            }
+        }
+        else
+        {
+            _isMouseOver = false;
+            if (ActivatedObject != null && ActivatedObject.activeInHierarchy == true)
+            {
+                ActivatedObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SetState(bool state) => _isActive = state;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (NeedsLineOfSight == true)
+            {
+                Vector3 playerPos = other.gameObject.transform.position;
+                float distance = Vector3.Distance(playerPos, gameObject.transform.position);
+                RaycastHit[] hits = Physics.RaycastAll(playerPos, gameObject.transform.position - playerPos, distance);
+                if (hits.Length > 0)
+                {
+                    if (hits[0].collider.gameObject == gameObject)
+                    {
+                        OnEnter?.Invoke();
+                        if (IsOneShot == true)
+                        {
+                            GetComponent<SphereCollider>().enabled = false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                OnEnter?.Invoke();
+                if (IsOneShot == true)
+                {
+                    GetComponent<SphereCollider>().enabled = false;
+                }
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (NeedsLineOfSight == true)
+            {
+                Vector3 playerPos = other.gameObject.transform.position;
+                float distance = Vector3.Distance(playerPos, gameObject.transform.position);
+                RaycastHit[] hits = Physics.RaycastAll(playerPos, gameObject.transform.position - playerPos, distance);
+                if (hits.Length > 0)
+                {
+                    if (hits[0].collider.gameObject == gameObject)
+                    {
+                        OnExit?.Invoke();
+                        if (IsOneShot == true)
+                        {
+                            GetComponent<SphereCollider>().enabled = false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                OnExit?.Invoke();
+                if (IsOneShot == true)
+                {
+                    GetComponent<SphereCollider>().enabled = false;
+                }
+            }
+        }
+    }
+}
